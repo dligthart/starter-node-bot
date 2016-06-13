2f549c894042fbe636e85739a8a87ee864bcc712
Dave Ligthart <dligthart@gmail.com>
2016-06-13 22:12:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
+controller.hears(['hello', 'hi', 'yo', 'whatsup'], ['direct_mention'], function (bot, message) {
   bot.reply(message, 'Hello.')
 })
 

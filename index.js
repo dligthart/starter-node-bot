c5ca196abcc64e8cff56d5eaf596e015b78c8d5f
System Administrator <dligthart@gmail.com>
2016-06-17 11:10:16 +0200

    update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var Botkit = require('botkit');
 var Witbot = require('witbot');
 var witbot = Witbot('KJN5XTUXGTW27DC7VJ4Y64QX6N7BZXA5');
 var token = process.env.SLACK_TOKEN;
-var controller = Botkit.slackbot({ debug: false })
+var controller = Botkit.slackbot({ debug: true })
 
-controller.spawn({ token: slackToken }).startRTM(function (err, bot, payload) {
+controller.spawn({ token: token }).startRTM(function (err, bot, payload) {
   if (err) throw new Error('Error connecting to Slack: ', err)
   console.log('Connected to Slack')
 })

38fdcb6ba5870e093e66b9bfba72507a8dc9420a
System Administrator <dligthart@gmail.com>
2016-06-17 10:40:49 +0200

    update

--- index.js
+++ index.js
@@ -1,91 +1,43 @@
 var Botkit = require('botkit');
-
-var wit = require('botkit-middleware-witai')({
-    token: 'KJN5XTUXGTW27DC7VJ4Y64QX6N7BZXA5'
-});
+var Witbot = require('witbot');
+var witbot = Witbot('KJN5XTUXGTW27DC7VJ4Y64QX6N7BZXA5');
 
 var token = process.env.SLACK_TOKEN;
 
-var controller = Botkit.slackbot({
-  // reconnect to Slack RTM when connection goes bad
-  retry: Infinity,
-  debug: false
-});
+var slackToken = process.env.SLACK_TOKEN
+var witbot = Witbot(process.env.WIT_TOKEN)
+var controller = Botkit.slackbot({ debug: false })
 
-controller.middleware.receive.use(wit.receive);
-
-// Assume single team mode if we have a SLACK_TOKEN
-if (token) {
-  console.log('Starting in single-team mode')
-  controller.spawn({
-    token: token
-  }).startRTM(function (err, bot, payload) {
-    if (err) {
-      throw new Error(err);
-    }
-
-    console.log('Connected to Slack RTM');
-  });
-
-// Otherwise assume multi-team mode - setup beep boop resourcer connection
-} else {
-  console.log('Starting in Beep Boop multi-team mode');
-  require('beepboop-botkit').start(controller, { debug: true });
-}
-
-/*controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!");
-});*/
-
-controller.hears(['hello'], 'direct_message', wit.hears, function(bot, message) {
-	bot.reply(message, 'Hello. from wit');
-});
-
-/*controller.hears(['hello', 'hi', 'yo', 'whatsup'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.');
-});
-
-controller.hears(['I would like to create an account', 'Go', 'Let\'s start'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Sure, let\'s do it! What\'s your name?');
+controller.spawn({ token: token }).startRTM(function (err, bot, payload) {
+  if (err) throw new Error('Error connecting to Slack: ', err)
+  console.log('Connected to Slack')
 })
 
-controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.');
-  bot.reply(message, 'It\'s nice to talk to you directly.');
-});
-*/
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:');
-});
-
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help);
-})
-
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
+controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
+  var wit = witbot.process(message.text, bot, message)
+  wit.hears('hello', 0.53, function (bot, message, outcome) {
+    bot.startConversation(message, function (_, convo) {
+      convo.say('Hello!');
+      convo.ask('How are you?', function (response, convo) {
+        witbot.process(response.text)
+          .hears('good', 0.5, function (outcome) {
+            convo.say('I am so glad to hear it!');
+            convo.next();
+          })
+          .hears('bad', 0.5, function (outcome) {
+            convo.say('I\'m sorry, that is terrible');
+            convo.next();
+          })
+          .otherwise(function (outcome) {
+            convo.say('I\'m cofused');
+            convo.repeat();
+            convo.next();
+          });
+      });
+    });
   });
-});
 
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+  wit.otherwise(function (bot, message) {
+    bot.reply(message, 'You are so intelligent, and I am so simple. I don\'t understand');
+  });
 });

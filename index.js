4dd6b7cef89e60c4d8bec9e7b47b445fd4dfe2f8
System Administrator <dligthart@gmail.com>
2016-06-17 12:01:13 +0200

    update

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var Botkit = require('botkit');
 var Witbot = require('witbot');
 var witbot = Witbot('KJN5XTUXGTW27DC7VJ4Y64QX6N7BZXA5');
-var token = process.env.SLACK_TOKEN;
-var controller = Botkit.slackbot({ debug: true })
+var slackToken = 'xoxp-23885891238-23890920277-50539946705-f3026a4d17';
+var token = process.env.SLACK_TOKEN; //slackToken; //
+var controller = Botkit.slackbot({ debug: false });
 
 controller.spawn({ token: token }).startRTM(function (err, bot, payload) {
-  if (err) throw new Error('Error connecting to Slack: ', err)
-  console.log('Connected to Slack')
-})
+  if (err) throw new Error('Error connecting to Slack: ', err);
+  console.log('Connected to Slack');
+});
 
-controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
+controller.hears(['hi'], ['direct_message', 'direct_mention'], function (bot, evt) {
+  bot.reply(evt, 'hello from bot')
+});
+
+controller.hears(['.*'], ['direct_message', 'direct_mention'], function (bot, message) {
   var wit = witbot.process(message.text, bot, message)
-  wit.hears('hello', 0.53, function (bot, message, outcome) {
+	console.log(message.text);
+  wit.hears('greeting', 0.53, function (bot, message, outcome) {
 		console.log(message, outcome);
     bot.startConversation(message, function (_, convo) {
       convo.say('Hello!');
